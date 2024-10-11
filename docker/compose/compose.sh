e0a13084afa4f15ecfdf83baab84c8b66e014af5
rdasilva01 <rodrigods2001@gmail.com>
2024-10-11 12:12:47 +0200

    Fix compose docker name

--- docker/compose/compose.sh
+++ docker/compose/compose.sh
@@ -4,10 +4,12 @@ PLATFORM="$(uname -m)"
 
 echo "Platform $PLATFORM detected"
 
+xhost +
+
 if [[ $PLATFORM == "aarch64" ]]; then
     docker compose -f docker-compose-aarch64.yml up -d
 else
     docker compose -f docker-compose.yml up -d
 fi
 
-docker exec -it as2_container /bin/bash
+docker exec -it a2rl_container /bin/bash
